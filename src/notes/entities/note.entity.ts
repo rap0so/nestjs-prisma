11cfadfd90a7d8aa1adcbb7fff83564b9dac1d0d
rap0so <william.s.godoy53@gmail.com>
2025-05-05 21:36:08 +0200

    Create Note entity

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,1 +1,10 @@
-export class Note {}
+import { ApiProperty } from '@nestjs/swagger';
+import { CreateNoteDto } from 'src/notes/dto/create-note.dto';
+
+export class Note extends CreateNoteDto {
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
