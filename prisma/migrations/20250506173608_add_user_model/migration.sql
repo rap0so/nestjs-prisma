92f20c76236a78b6bd4afcbce33c754099418393
rap0so <william.s.godoy53@gmail.com>
2025-05-06 19:44:37 +0200

    Create User model

---
+++ prisma/migrations/20250506173608_add_user_model/migration.sql
@@ -0,0 +1,20 @@
+-- AlterTable
+ALTER TABLE "Note" ADD COLUMN     "authorId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_login_key" ON "User"("login");
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,27 +3,50 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+  // create an dummy author
+  const user1 = await prisma.user.upsert({
+    where: { login: 'dummy' },
+    update: {},
+    create: {
+      login: 'dummy',
+      name: 'Dummy',
+      password: '123456',
+    },
+  });
+
+  const user2 = await prisma.user.upsert({
+    where: { login: 'dummier' },
+    update: {},
+    create: {
+      login: 'dummier',
+      name: 'Dummier',
+      password: '123456',
+    },
+  });
+
   // create two dummy articles
   const post1 = await prisma.note.upsert({
     where: { title: 'My first note on prisma' },
-    update: {},
+    update: { authorId: user1.id },
     create: {
       title: 'My first note on prisma',
       body: 'This is the first note I saved on my postgreSQL using Prisma with NestJs',
       description: 'This is the first note',
       published: false,
+      authorId: 1,
     },
   });
 
   const post2 = await prisma.note.upsert({
     where: { title: 'My second note on prisma' },
-    update: {},
+    update: { authorId: user2.id },
     create: {
       title: 'My second note on prisma',
       body: 'RADA RADA RADA WADA WADA WADA',
       description:
         'Sabe que esse dia ta sendo realmente bem legal?\n to feliz que isso ta funcionando tão facil 🤓',
       published: true,
+      authorId: 1,
     },
   });
 
