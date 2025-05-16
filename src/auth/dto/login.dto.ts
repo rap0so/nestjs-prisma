1ae6d3b892dd35a03e4d45c0931693e902dec43b
rap0so <william.s.godoy53@gmail.com>
2025-05-16 21:09:51 +0200

    Create login dto

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+
+export class Login {
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty()
+  email: string;
+
+  @IsStrongPassword({
+    minLength: 8,
+    minNumbers: 1,
+    minUppercase: 1,
+  })
+  @IsNotEmpty()
+  password: string;
+}
